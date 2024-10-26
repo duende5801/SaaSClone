27a8820177b30f705ed691dcc51934179d6384c1
David Abdallah <56611595+duende5801@users.noreply.github.com>
2024-10-26 13:22:07 -0700

    homepage

--- src/app/(marketing)/_components/NavBar.tsx
+++ src/app/(marketing)/_components/NavBar.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export function NavBar() {
   return (
-    <header className="flex py-6 shadow-xl fixted top-0 w-full z-10 bg-background/95">
+    <header className="flex py-6 shadow-xl fixed top-0 w-full z-10 bg-background/95">
       <nav className="flex items-cent gap-10 container font-semibold">
         <Link href="/" className="mr-auto">
           <BrandLogo />
